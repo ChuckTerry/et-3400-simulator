ce2feb539a32ca3d04628e7f6a4386f7a203d2f1
ChuckTerry <Chuck.Terry@Outlook.com>
2024-01-29 04:56:35 -0500

    import chain

---
+++ src/programs/heathkit/heathkit.js
@@ -0,0 +1,5 @@
+import { sample1 } from './et-3400-manual/sample1.js';
+
+export const heathkit = {
+  sample1: sample1
+};
---
+++ src/programs/programs.js
@@ -0,0 +1,5 @@
+import { heathkit } from './heathkit/heathkit.js';
+
+export const programs = {
+  heathkit: heathkit
+};

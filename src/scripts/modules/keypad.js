abb331906a9b1384b493f8f23c961f6d013d1b57
ChuckTerry <Chuck.Terry@Outlook.com>
2024-02-16 23:41:27 -0500

    Local KeyPadKey Class

--- src/scripts/modules/keypad.js
+++ src/scripts/modules/keypad.js
@@ -1,189 +1,33 @@
-export const keypad = {
-  DO: {
-    keyboardActivation: 'D',
-    keyCode: 0x06,
-    abortControllers: {
+export class KeypadKey {
+  constructor(keyboardActivation, keyCode, selector, simulator) {
+    this.keyboardActivation = keyboardActivation;
+    this.keyCode = keyCode;
+    this.selector = selector;
+    this.simulator = simulator;
+    this.abortControllers = {
       click: null,
       keyUpDown: null,
       touchStartEnd: null
-    },
-    selector: '#button-do',
-    simulator: '#do'
-  },
-  EXAM: {
-    keyboardActivation: 'E',
-    keyCode: 0x05,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-exam',
-    simulator: '#exam'
-  },
-  FWD: {
-    keyboardActivation: 'F',
-    keyCode: 0x03,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-fwd',
-    simulator: '#fwd'
-  },
-  AUTO: {
-    keyboardActivation: 'A',
-    keyCode: 0x16,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-auto',
-    simulator: '#auto'
-  },
-  BACK: {
-    keyboardActivation: 'B',
-    keyCode: 0x15,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-back',
-    simulator: '#back'
-  },
-  CHAN: {
-    keyboardActivation: 'C',
-    keyCode: 0x13,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-chan',
-    simulator: '#chan'
-  },
-  RTI: {
-    keyboardActivation: '7',
-    keyCode: 0x26,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-rti',
-    simulator: '#rti'
-  },
-  SS: {
-    keyboardActivation: '8',
-    keyCode: 0x25,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-ss',
-    simulator: '#ss'
-  },
-  BR: {
-    keyboardActivation: '9',
-    keyCode: 0x23,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-br',
-    simulator: '#br'
-  },
-  INDEX: {
-    keyboardActivation: '4',
-    keyCode: 0x36,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-index',
-    simulator: '#index'
-  },
-  CC: {
-    keyboardActivation: '5',
-    keyCode: 0x35,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-cc',
-    simulator: '#cc'
-  },
-  SP: {
-    keyboardActivation: '6',
-    keyCode: 0x33,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-sp',
-    simulator: '#sp'
-  },
-  ACCA: {
-    keyboardActivation: '1',
-    keyCode: 0x46,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-acca',
-    simulator: '#acca'
-  },
-  ACCB: {
-    keyboardActivation: '2',
-    keyCode: 0x45,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-accb',
-    simulator: '#accb'
-  },
-  PC: {
-    keyboardActivation: '3',
-    keyCode: 0x43,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-pc',
-    simulator: '#pc'
-  },
-  ZERO: {
-    keyboardActivation: '0',
-    keyCode: 0x56,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-zero',
-    simulator: '#zero'
-  },
-  RESET: {
-    keyboardActivation: 'ESCAPE',
-    keyCode: null,
-    abortControllers: {
-      click: null,
-      keyUpDown: null,
-      touchStartEnd: null
-    },
-    selector: '#button-reset',
-    simulator: '#reset'
+    };
   }
+}
+
+export const keypad = {
+  ACCA: new KeypadKey('1', 0x46, '#button-acca', '#acca'),
+  ACCB: new KeypadKey('2', 0x45, '#button-accb', '#accb'),
+  AUTO: new KeypadKey('A', 0x16, '#button-auto', '#auto'),
+  BACK: new KeypadKey('B', 0x15, '#button-back', '#back'),
+  BR: new KeypadKey('9', 0x23, '#button-br', '#br'),
+  CC: new KeypadKey('5', 0x35, '#button-cc', '#cc'),
+  CHAN: new KeypadKey('C', 0x13, '#button-chan', '#chan'),
+  DO: new KeypadKey('D', 0x06, '#button-do', '#do'),
+  EXAM: new KeypadKey('E', 0x05, '#button-exam', '#exam'),
+  FWD: new KeypadKey('F', 0x03, '#button-fwd', '#fwd'),
+  INDEX: new KeypadKey('4', 0x36, '#button-index', '#index'),
+  PC: new KeypadKey('3', 0x43, '#button-pc', '#pc'),
+  RESET: new KeypadKey('ESCAPE', null, '#button-reset', '#reset'),
+  RTI: new KeypadKey('7', 0x26, '#button-rti', '#rti'),
+  SP: new KeypadKey('6', 0x33, '#button-sp', '#sp'),
+  SS: new KeypadKey('8', 0x25, '#button-ss', '#ss'),
+  ZERO: new KeypadKey('0', 0x56, '#button-zero', '#zero')
 };

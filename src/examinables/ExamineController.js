4d4b7e322c892c06a6f3f98e426422c995861688
ChuckTerry <Chuck.Terry@Outlook.com>
2024-02-27 20:20:39 -0500

    Update Pathing; Move to own folder

---
+++ src/examinables/ExamineController.js
@@ -0,0 +1,29 @@
+import { examinableNameArray, examinableCount } from './examinableNameArray.js';
+
+export class ExamineController {
+  constructor() {
+    this.examinables = new Map();
+    const path = './html/';
+    for (let index = 0; index < examinableCount; index++) {
+      const name = examinableNameArray[index];
+      const filePath = `${path}${name}.html`;
+      fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+          this.examinables.set(name, data);
+        });
+    }
+  }
+
+  display(element) {
+    if (element.classList.contains('examinable') === false) {
+      return;
+    }
+    const name = element.dataset.name;
+    let content = this.examinables.has(name)
+      ? this.examinables.get(name) 
+      : this.examinables.get('not-found');
+    document.querySelector('#examine-template').innerHTML = content;
+    document.querySelector('#examine-dialog').showModal();
+  }
+}

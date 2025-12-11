3cb7b4e29cb8e34a0879a32f3c9ca8b4b799f369
Chuck Terry <Chuck.Terry@Outlook.com>
2025-12-11 22:20:46 +0000

    Add my Address Looper

---
+++ src/programs/thirdParty/addressLoop.js
@@ -0,0 +1,21 @@
+import { hexLoadStringFromAnnotatedCode } from '../../../scripts/modules/util.js';
+
+export const addressLoopAnnotated = `
+
+				**      ADDRLP - ADDRESS LOOP
+				*
+				*		DISPLAYS ALL MEMORY VALUES IN A LOOP
+				*       STARTING WITH THE ADDRESS AT #NXTADD
+
+0000  CE FC00   ADDRLP  LDX     RESET,#NXTADD  START WITH THE MONITOR
+0003  BD FCF9           JSR     MEM            DISPLAY ADDR AND VALUE
+0006  08                INX                    
+0007  FF 0001           STX     #NXTADD        STORE NEXT ADDRESS
+000A  CE A100           LDX                    ADJUST WAIT TIME
+000D  09        DELAY   DEX                    
+000E  26 FD             BNE     DELAY          TWIDDLE THUMBS
+0010  20 EE             BRA     ADDRLP         ON TO THE NEXT ONE
+
+`;
+
+export const addressLoop = hexLoadStringFromAnnotatedCode(addressLoopAnnotated);

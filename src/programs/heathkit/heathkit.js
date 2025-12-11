deb12e002b7f76ef744624eab21131c90b9cc5f4
Chuck Terry <Chuck.Terry@Outlook.com>
2025-12-11 22:21:39 +0000

    Bundle Samples

--- src/programs/heathkit/heathkit.js
+++ src/programs/heathkit/heathkit.js
@@ -1,7 +1,19 @@
 import { sample1 } from './et-3400-manual/sample1.js';
+import { sample2 } from './et-3400-manual/sample2.js';
+import { sample3 } from './et-3400-manual/sample3.js';
+import { sample4 } from './et-3400-manual/sample4.js';
+import { sample5 } from './et-3400-manual/sample5.js';
+import { sample6 } from './et-3400-manual/sample6.js';
+import { sample7 } from './et-3400-manual/sample7.js';
 import { ROM } from './rom.js';
 
 export const heathkit = {
-  ROM: ROM,
-  sample1: sample1
+    ROM: ROM,
+    sample1: sample1,
+    sample2: sample2,
+    sample3: sample3,
+    sample4: sample4,
+    sample5: sample5,
+    sample6: sample6,
+    sample7: sample7
 };
